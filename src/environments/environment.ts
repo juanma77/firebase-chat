0cd40e83d81dae79921b55148212e5ae470b2336
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-08-06 13:48:12 -0500

    angular firestore library added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCAitB4bbUnDDmPjLvlfTNmIyhVcGzCo3k",
+    authDomain: "fir-chat-8bd03.firebaseapp.com",
+    databaseURL: "https://fir-chat-8bd03.firebaseio.com",
+    projectId: "fir-chat-8bd03",
+    storageBucket: "fir-chat-8bd03.appspot.com",
+    messagingSenderId: "449263855797",
+    appId: "1:449263855797:web:92a4b11f94ca10b5ba76fc",
+    measurementId: "G-33PMFNL3Z2"
+  }
 };
 
 /*
